93e099dbaf66c9340e9ab3db24d43180941909f8
Lukas Sparrow <lukas.sparrow@gmail.com>
2014-11-27 15:46:09 +0100

    komit

--- junction.py
+++ junction.py
@@ -9,6 +9,99 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 
+
+
+
+
+
+
+def m_low(var):
+    if var > 10:
+        return 0.0
+    else:
+        return (-1/10.0)*var + 1
+
+
+def m_middle(var):
+    if var < 5 or var > 35:
+        return 0.0
+
+    if 5 <= var < 20:
+        return (1/15.0)*var - (1/3.0)
+
+    if 20 <= var <= 35:
+        return (-1/15.0)*var + (7/3.0)
+
+
+def m_high(var):
+    if var < 30:
+        return 0.0
+
+    if var > 40:
+        return 1.0
+
+    if 30 <= var <= 40:
+        return (1/10.0)*var - (3.0)
+
+
+
+
+class Fuzzy(object):
+    def __init__(self, var):
+        self.var = var
+        self.L = m_low(var)
+        self.M = m_middle(var)
+        self.H = m_high(var)
+
+
+def NOT(v):
+    return 1-v
+
+def AND(*args):
+    return min(args)
+
+
+def decide(aktualni, vedlejsi):
+    if(aktualni == 0 and vedlejsi == 0):
+        return False
+
+    X = Fuzzy(aktualni)
+    Y = Fuzzy(vedlejsi)
+    R = Fuzzy(5*(1.0*vedlejsi/aktualni if aktualni!=0 else 9999)) #fixme
+
+    F = []
+    T = []
+
+    F.append(R.L)
+    T.append(R.M)
+    T.append(R.H)
+
+    """
+    F.append(X.H)
+    T.append(AND(X.L, NOT(Y.L)))
+    F.append(AND(X.M, NOT(Y.H)))
+    T.append(AND(X.M, Y.H))
+    """
+
+    return max(F) < max(T)
+
+def dlouhy_interval(aktualni, vedlejsi):
+    kratky = []
+    dlouhy = []
+
+    X = Fuzzy(aktualni)
+    Y = Fuzzy(vedlejsi)
+
+    kratky.append(X.L)
+    kratky.append(NOT(Y.L))
+    dlouhy.append(X.H)
+    dlouhy.append(Y.L)
+
+    return max(dlouhy) > max(kratky)
+
+
+
+
 verbose = False
 pretty_print = False
 
@@ -19,10 +112,21 @@ served_last = 0
 waited_average = 0
 queue_wa = {"we": (0, 0), "ew": (0, 0), "ns": (0, 0), "sn": (0, 0)}
 
+karlova = None
+j_karlova = None
+# za 24 hodin:
+# 5500 aut tezkych/nakladnich (20m na auto)
+# 32000 aut osobnich (6m na auto)
+# 500m, 2 pruhy --> kapacita 125 aut
+j_karlova_capacity = 125
+k_c = 0
+k_p_c = 0
+
 monitor_data = []
 
 start_hour = 0
-day_coef = [0.05,0.01,0.01,0.01,0.1,0.5,1.0,2.0,3.0,3.5,2.0,1.0,1.0,1.5,2.0,3.0,3.5,3.0,2.0,1.0,0.75,0.5,0.25,0.1]
+#day_coef = [0.05,0.01,0.01,0.05,0.1,0.5,1.0,2.0,3.0,3.5,2.0,1.0,1.0,1.5,2.0,3.0,3.5,3.0,2.0,1.0,0.75,0.5,0.25,0.1]
+day_coef = [0.05,0.01,0.01,0.05,0.1,0.5,1.0,2.0,3.0,3.5,2.5,2.2,2.0,2.2,2.5,3.0,3.5,3.0,2.0,1.0,0.75,0.5,0.25,0.1]
 cars = []
 
 waited_average_actual_interval = 0
@@ -41,6 +145,7 @@ class Car(object):
 
     def go(self):
         global served, verbose, waited_average, queue_wa, waited_average_actual_interval, s_actual_interval
+        global k_c, k_p_c
         if verbose:
             print '%d: %d arrived, will go %s' % (self.env.now, self.i, self.direction)
 
@@ -53,34 +158,25 @@ class Car(object):
             yield req  # cekani na prvni misto
             
             #uvolnilo se misto pred autem, auto se posunuje, trva chvili nez se rozjede
-            yield self.env.timeout(random.uniform(1,3))  # X sekund trva rozjizdeni aut
+            yield self.env.timeout(random.uniform(1.1, 2))  # X sekund trva rozjizdeni aut
+            #yield self.env.timeout(4)  # X sekund trva rozjizdeni aut
 
             while True:
                 if verbose:
                     print '%d: %d is waiting for green' % (self.env.now, self.i)
 
-                if self.traffic_light.state == "green":
+                if self.traffic_light.state == "green" and (self.direction != "we" or len(j_karlova.queue) < j_karlova_capacity):
                     #jestli je zelena, tak pokracuje bez cekani (nerozjizdi se, jede dal)
                     break
                 else:
-                    #ceka na semaforu
-                    yield self.env.process(self.traffic_light.wait()) 
-                    #chvili trva nez se rozjede
-                    yield self.env.timeout(random.uniform(1,3))
-
-
-                """
-                #cekani na zelenou na semaforu
-                yield self.env.process(self.traffic_light.wait()) 
-                if verbose:
-                    print '%d: %d is free to go %s' % (self.env.now, self.i, self.direction)
-                yield self.env.timeout(random.uniform(1,3))  # X sekund trva rozjizdeni aut
-
-                break
-                if self.traffic_light.state == "green":
-                    break
-                """
-
+                    if self.direction == "we" and self.traffic_light.state == "green":
+                        #ma zelenou, ale chce pokracovat smer karlova, ktery je plny
+                        yield self.traffic_light.switch_event
+                    else:
+                        #ceka na semaforu
+                        yield self.env.process(self.traffic_light.wait()) 
+                        #chvili trva nez se rozjede
+                        yield self.env.timeout(random.uniform(1.1, 2))
 
             waited = self.env.now - started
             waited_average += waited
@@ -94,20 +190,30 @@ class Car(object):
             served += 1
             s_actual_interval += 1
 
+        if self.direction == "we" and False:
+            with j_karlova.request() as req:
+                yield req
+                yield self.env.timeout(random.uniform(1.1, 2))
+                while True:
+                    if karlova.state == "green":
+                        break
+                    else:
+                        yield self.env.process(karlova.wait())
+                        yield self.env.timeout(random.uniform(1.1, 2))
+
 
 class TrafficLight(object):
-    def __init__(self, env, direction, init_state, starting):
+    def __init__(self, env, direction, init_state):
         self.env = env
         self.direction = direction
         self.state = init_state
-        self.starting = starting
         self.switch_event = env.event()
 
     def wait(self):
         # pokud je na semaforu cervena, cekat na event prepnuti na zelenou
         if self.state == "red":
             yield self.switch_event  # cekani na zelenou, spoustena TimedControlLogic
-            yield self.env.timeout(self.starting)  # X sekund trva rozjizdeni aut
+            #yield self.env.timeout(self.starting)  # X sekund trva rozjizdeni aut
         # skonci proces wait, auto muze jet
 
 
@@ -127,6 +233,7 @@ class TimedControlLogic(object):
             old = o_cnt
             o_cnt = (o_cnt + 1) % len(order)
 
+            #yield self.env.timeout(self.light_time*(0.5+day_coef[hour]))  # cekani s rozsvicenymi svetly
             yield self.env.timeout(self.light_time)  # cekani s rozsvicenymi svetly
 
             # zastaveni jednoho smeru
@@ -145,6 +252,75 @@ class TimedControlLogic(object):
             self.traffic_lights[order[o_cnt][0]].switch_event = self.env.event()
             self.traffic_lights[order[o_cnt][1]].switch_event.succeed()
             self.traffic_lights[order[o_cnt][1]].switch_event = self.env.event()
+
+
+class FuzzyControlLogic(object):
+    def __init__(self, env, light_time, offset_time, tl_we, tl_ew, tl_ns, tl_sn, j_we, j_ew, j_ns, j_sn):
+        self.env = env
+        self.traffic_lights = {"we": tl_we, "ew": tl_ew, "ns": tl_ns, "sn": tl_sn}
+        self.junctions = {"we": j_we, "ew": j_ew, "ns": j_ns, "sn": j_sn}
+        self.action = env.process(self.run())
+        self.offset_time = offset_time
+
+    def run(self):
+        global verbose
+        order = [("we", "ew"), ("ns", "sn")]
+        o_cnt = 0
+        old = 1
+        while True:
+
+            aktualni = len(self.junctions[order[o_cnt][0]].queue) + len(self.junctions[order[o_cnt][1]].queue) + self.junctions[order[o_cnt][0]].count + self.junctions[order[o_cnt][1]].count
+            vedlejsi = len(self.junctions[order[old][0]].queue) + len(self.junctions[order[old][1]].queue) + self.junctions[order[old][0]].count + self.junctions[order[old][1]].count
+
+            interval___ = 30 if dlouhy_interval(aktualni, vedlejsi) else 5
+            print aktualni, vedlejsi, interval___
+
+            if decide(aktualni, vedlejsi):
+                print "preblikavam"
+                old = o_cnt
+                o_cnt = (o_cnt + 1) % len(order)
+
+                # zastaveni jednoho smeru
+                self.traffic_lights[order[old][0]].state = "red"
+                self.traffic_lights[order[old][1]].state = "red"
+
+                yield self.env.timeout(self.offset_time)  # offset pro dojezd aut
+
+                if verbose:
+                    print '%d: %s are good to go' % (self.env.now, " and ".join(order[o_cnt]))
+                # pusteni druheho smeru
+                self.traffic_lights[order[o_cnt][0]].state = "green"
+                self.traffic_lights[order[o_cnt][1]].state = "green"
+                # spusti se udalosti tech, co cekali na cervenou
+                self.traffic_lights[order[o_cnt][0]].switch_event.succeed()
+                self.traffic_lights[order[o_cnt][0]].switch_event = self.env.event()
+                self.traffic_lights[order[o_cnt][1]].switch_event.succeed()
+                self.traffic_lights[order[o_cnt][1]].switch_event = self.env.event()
+
+            yield self.env.timeout(5)
+
+
+class TimdeControlLogicKarlova(object):
+    def __init__(self, env, light_time, tl):
+        self.env = env
+        self.tl = tl
+        self.action = env.process(self.run())
+        self.light_time = light_time
+
+    def run(self):
+        cnt = 0
+        color = ("green", "red")
+        #color = ("red", "red")
+        while True:
+            yield self.env.timeout(self.light_time)
+            self.tl.state = color[cnt]
+            if (self.tl.state == "green"):
+                self.tl.switch_event.succeed()
+                self.tl.switch_event = self.env.event()
+            cnt = (cnt+1)%2
+
+
+
 
 
 def car_generator(env, interval, junction, traffic_light, direction):
@@ -183,10 +359,14 @@ def monitor(env, interval, j_we, j_ew, j_ns, j_sn):
                 queue_wa["sn"][0]/float(queue_wa["sn"][1]) if queue_wa["sn"][1] else 0
             )
         else:
+            """
             print "%d %.1f %.1f %d %d %d %d" % (
                 env.now, ((s / float(c) * 100) if c != 0 else 0), (waited_average / float(served)) if served != 0 else 0,
                 len(j_we.queue), len(j_ew.queue), len(j_ns.queue), len(j_sn.queue)
             )
+            """
+            pass
+            
 
         monitor_data.append((
                 env.now, ((s / float(c) * 100) if c != 0 else 0), (waited_average / float(served)) if served != 0 else 0,
@@ -211,50 +391,66 @@ def plot_data(running_time):
     plt.subplot(311)
     plt.plot(time, data[:,2])
     plt.ylabel('average waiting time cumulative')
+    plt.grid()
 
     plt.subplot(312)
     plt.plot(np.array(range(0,running_time, 600))/60, np.array(waited_average_actual)/60.0)
     plt.ylabel('average waiting time')
+    plt.grid()
 
     plt.subplot(313)
     plt.plot(time, cars)
     plt.ylabel('cars arrived')    
+    plt.grid()
+
     plt.show()
 
 
 
 def main(running_time):
-    global served, counter, waited_average
+    global served, counter, waited_average, j_karlova, karlova
     env = simpy.Environment()
     # env = simpy.rt.RealtimeEnvironment(factor=2.0)
 
     # semafory z jednotlivych smeru
-    tl_we = TrafficLight(env, "we", "green", 4)
-    tl_ew = TrafficLight(env, "ew", "green", 4)
-    tl_ns = TrafficLight(env, "ns", "red", 4)
-    tl_sn = TrafficLight(env, "sn", "red", 4)
-    # rizeni prepinani semaforu
-    TimedControlLogic(env, int(sys.argv[2]), 5, tl_we, tl_ew, tl_ns, tl_sn)
+    tl_we = TrafficLight(env, "we", "green")
+    tl_ew = TrafficLight(env, "ew", "green")
+    tl_ns = TrafficLight(env, "ns", "red")
+    tl_sn = TrafficLight(env, "sn", "red")
+
+    karlova = TrafficLight(env, "karlova", "red")
 
     # fronta na prvni misto
     j_we = simpy.Resource(env, capacity=1)
     j_ew = simpy.Resource(env, capacity=1)
     j_ns = simpy.Resource(env, capacity=1)
     j_sn = simpy.Resource(env, capacity=1)
+    j_karlova = simpy.Resource(env, capacity=1)
+
+    # rizeni prepinani semaforu
+    #TimedControlLogic(env, int(sys.argv[2]), 5, tl_we, tl_ew, tl_ns, tl_sn)
+
+    FuzzyControlLogic(env, int(sys.argv[2]), 5, tl_we, tl_ew, tl_ns, tl_sn, j_we, j_ew, j_ns, j_sn)
+    
+
+    TimdeControlLogicKarlova(env, int(sys.argv[2]), karlova)
+
 
     # generovani prijezdu aut
-    podivny_parametr = 20
+    podivny_parametr_we = 10
+    podivny_parametr_ns = 40
 
-    env.process(car_generator(env, podivny_parametr, j_we, tl_we, "we"))
-    env.process(car_generator(env, podivny_parametr, j_ew, tl_ew, "ew"))
-    env.process(car_generator(env, podivny_parametr, j_ns, tl_ns, "ns"))
-    env.process(car_generator(env, podivny_parametr, j_sn, tl_sn, "sn"))
+    env.process(car_generator(env, podivny_parametr_we, j_we, tl_we, "we"))
+    env.process(car_generator(env, podivny_parametr_we, j_ew, tl_ew, "ew"))
+    env.process(car_generator(env, podivny_parametr_ns, j_ns, tl_ns, "ns"))
+    env.process(car_generator(env, podivny_parametr_ns, j_sn, tl_sn, "sn"))
 
     # sbirani statistik o simulaci
     env.process(monitor(env, 60, j_we, j_ew, j_ns, j_sn))
 
     env.run(until=running_time)
 
+    print "projelo: %d" % (counter)
     plot_data(running_time)
 
 if __name__ == "__main__":
